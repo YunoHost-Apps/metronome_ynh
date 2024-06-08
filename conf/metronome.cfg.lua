4d97971257acf44c5967bdfa861ed46f8650d22a
Alexandre Aubin <alex.aubin@mailoo.org>
2024-06-08 17:13:07 +0200

    Cosmetics

--- conf/metronome.cfg.lua
+++ conf/metronome.cfg.lua
@@ -22,51 +22,51 @@
 -- It looks for mod_modulename.lua in the plugins folder, so make sure that exists too.
 
 modules_enabled = {
-	-- Generally required
-		"roster"; -- Allow users to have a roster. Recommended.
-		"saslauth"; -- Authentication for clients. Recommended if you want to log in.
-		"tls"; -- Add support for secure TLS on c2s/s2s connections
-		"disco"; -- Service discovery
+    -- Generally required
+    "roster"; -- Allow users to have a roster. Recommended.
+    "saslauth"; -- Authentication for clients. Recommended if you want to log in.
+    "tls"; -- Add support for secure TLS on c2s/s2s connections
+    "disco"; -- Service discovery
  
-	-- Not essential, but recommended
-		"private"; -- Private XML storage (for room bookmarks, etc.)
-		"vcard"; -- Allow users to set vCards
-		"pep"; -- Allows setting of mood, tune, etc.
-		"pubsub";  -- Publish-subscribe XEP-0060
-		"posix"; -- POSIX functionality, sends server to background, enables syslog, etc.
-		"bidi"; -- Enables Bidirectional Server-to-Server Streams.
+    -- Not essential, but recommended
+    "private"; -- Private XML storage (for room bookmarks, etc.)
+    "vcard"; -- Allow users to set vCards
+    "pep"; -- Allows setting of mood, tune, etc.
+    "pubsub";  -- Publish-subscribe XEP-0060
+    "posix"; -- POSIX functionality, sends server to background, enables syslog, etc.
+    "bidi"; -- Enables Bidirectional Server-to-Server Streams.
  
-	-- Nice to have
-		"version"; -- Replies to server version requests
-		"uptime"; -- Report how long server has been running
-		"time"; -- Let others know the time here on this server
-		"ping"; -- Replies to XMPP pings with pongs
-		"register"; -- Allow users to register on this server using a client and change passwords
-		"stream_management"; -- Allows clients and servers to use Stream Management
-		"stanza_optimizations"; -- Allows clients to use Client State Indication and SIFT
-		"message_carbons"; -- Allows clients to enable carbon copies of messages
-		"mam"; -- Enable server-side message archives using Message Archive Management
-		"push"; -- Enable Push Notifications via PubSub using XEP-0357
-		"lastactivity"; -- Enables clients to know the last presence status of an user
-		"adhoc_cm"; -- Allow to set client certificates to login through SASL External via adhoc
-		"admin_adhoc"; -- administration adhoc commands
-		"bookmarks"; -- XEP-0048 Bookmarks synchronization between PEP and Private Storage
-		"sec_labels"; -- Allows to use a simplified version XEP-0258 Security Labels and related ACDFs.
-		"privacy"; -- Add privacy lists and simple blocking command support
+    -- Nice to have
+    "version"; -- Replies to server version requests
+    "uptime"; -- Report how long server has been running
+    "time"; -- Let others know the time here on this server
+    "ping"; -- Replies to XMPP pings with pongs
+    "register"; -- Allow users to register on this server using a client and change passwords
+    "stream_management"; -- Allows clients and servers to use Stream Management
+    "stanza_optimizations"; -- Allows clients to use Client State Indication and SIFT
+    "message_carbons"; -- Allows clients to enable carbon copies of messages
+    "mam"; -- Enable server-side message archives using Message Archive Management
+    "push"; -- Enable Push Notifications via PubSub using XEP-0357
+    "lastactivity"; -- Enables clients to know the last presence status of an user
+    "adhoc_cm"; -- Allow to set client certificates to login through SASL External via adhoc
+    "admin_adhoc"; -- administration adhoc commands
+    "bookmarks"; -- XEP-0048 Bookmarks synchronization between PEP and Private Storage
+    "sec_labels"; -- Allows to use a simplified version XEP-0258 Security Labels and related ACDFs.
+    "privacy"; -- Add privacy lists and simple blocking command support
 
-		-- Other specific functionality
-		--"admin_telnet"; -- administration console, telnet to port 5582
-		--"admin_web"; -- administration web interface
-		"bosh"; -- Enable support for BOSH clients, aka "XMPP over Bidirectional Streams over Synchronous HTTP"
-		--"compression"; -- Allow clients to enable Stream Compression
-		--"spim_block"; -- Require authorization via OOB form for messages from non-contacts and block unsollicited messages
-		--"gate_guard"; -- Enable config-based blacklisting and hit-based auto-banning features
-		--"incidents_handling"; -- Enable Incidents Handling support (can be administered via adhoc commands)
-		--"server_presence"; -- Enables Server Buddies extension support
-		--"service_directory"; -- Enables Service Directories extension support
-		--"public_service"; -- Enables Server vCard support for public services in directories and advertises in features
-		--"register_api"; -- Provides secure API for both Out-Of-Band and In-Band registration for E-Mail verification
-		"websocket"; -- Enable support for WebSocket clients, aka "XMPP over WebSockets"
+    -- Other specific functionality
+    --"admin_telnet"; -- administration console, telnet to port 5582
+    --"admin_web"; -- administration web interface
+    "bosh"; -- Enable support for BOSH clients, aka "XMPP over Bidirectional Streams over Synchronous HTTP"
+    --"compression"; -- Allow clients to enable Stream Compression
+    --"spim_block"; -- Require authorization via OOB form for messages from non-contacts and block unsollicited messages
+    --"gate_guard"; -- Enable config-based blacklisting and hit-based auto-banning features
+    --"incidents_handling"; -- Enable Incidents Handling support (can be administered via adhoc commands)
+    --"server_presence"; -- Enables Server Buddies extension support
+    --"service_directory"; -- Enables Service Directories extension support
+    --"public_service"; -- Enables Server vCard support for public services in directories and advertises in features
+    --"register_api"; -- Provides secure API for both Out-Of-Band and In-Band registration for E-Mail verification
+    "websocket"; -- Enable support for WebSocket clients, aka "XMPP over WebSockets"
 };
 
 -- Server PID
@@ -99,10 +99,10 @@ csi_config_queue_all_muc_messages_but_mentions = false;
 
 -- Logging configuration
 log = {
-	info = "/var/log/__APP__/__APP__.log"; -- Change 'info' to 'debug' for verbose logging
-	error = "/var/log/__APP__/__APP__.err";
-	-- "*syslog"; -- Uncomment this for logging to syslog
-	-- "*console"; -- Log to the console, useful for debugging with daemonize=false
+    info = "/var/log/__APP__/__APP__.log"; -- Change 'info' to 'debug' for verbose logging
+    error = "/var/log/__APP__/__APP__.err";
+    -- "*syslog"; -- Uncomment this for logging to syslog
+    -- "*console"; -- Log to the console, useful for debugging with daemonize=false
 }
 
 ------ Components ------
@@ -111,7 +111,7 @@ log = {
 
 ---Set up a local BOSH service
 Component "localhost" "http"
-	modules_enabled = { "bosh" }
+    modules_enabled = { "bosh" }
 
 ----------- Virtual hosts -----------
 -- You need to add a VirtualHost entry for each domain you wish Metronome to serve.
