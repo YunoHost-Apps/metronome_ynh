649b2745f90a8c47bcc1e121e1eabe1349ef4d65
Félix Piédallu <felix@piedallu.me>
2024-07-01 16:11:56 +0200

    Build metronome in install_dir/sources because tmpdir might be too small

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -14,6 +14,23 @@ fi
 # PERSONAL HELPERS
 #=================================================
 
+_build_metronome() {
+	pushd "$install_dir/sources"
+		cp docker/config.unix ./
+		make generate_log.install send_mail.install install \
+			DESTDIR="$install_dir" \
+			BIN="$install_dir/bin" \
+			CONFIG="$install_dir/conf" \
+			MODULES="$install_dir/modules" \
+			SOURCE="$install_dir/lib" \
+			DATA="$data_dir" \
+			INSTALLEDCONFIG="$install_dir/conf" \
+			INSTALLEDMODULES="$install_dir/modules" \
+			INSTALLEDSOURCE="$install_dir/lib" \
+			INSTALLEDDATA="$data_dir"
+	popd
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
